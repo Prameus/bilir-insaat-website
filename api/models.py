9e1aade99a34c4d7a47f0451fd64ccb7158ac79e
Musa Karaaslan <musa_karaaslan2@hotmail.com>
2023-06-15 16:50:35 +0300

    fourth commit

--- api/models.py
+++ api/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class Reffrences(models.Model):
-    pass
+    baslik = models.CharField(max_length=150)
+    aciklama = models.TextField()
+    referans_fotografi = models.ImageField(upload_to="reference_images/% Y/% m/% d/")
+    date = models.DateField(auto_now=False, auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
--- bilir_insaat/settings.py
+++ bilir_insaat/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontend.apps.FrontendConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
