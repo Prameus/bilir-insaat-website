f1b58524f6793628a074f24b16cae6419285a1a2
Musa Karaaslan <musa_karaaslan2@hotmail.com>
2023-06-18 14:21:34 +0300

    about section update

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from .views import home
+from django.urls import path
+from .views import home, reference
 
 urlpatterns = [
     path('home/',home),
+    path('reference/',reference)
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request, *args, **kwargs):
     return render(request, 'home.html')
+
+def reference(request,*args,**kwargs):
+    return render(request, 'reference.html')
