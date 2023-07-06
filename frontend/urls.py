03197c0a086ee3f682480fcc10bc856dd895e76e
Enes <enesalkus@hotmail.com>
2023-07-06 22:57:32 +0300

    Update

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import home, reference, about
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home),
     path('reference/', reference),
     path('about/', about)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # pragma: nocover
