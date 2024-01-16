0530c9c3d61934f339a3904d8c7d2a28986cf4e3
Musa Karaaslan <musa_karaaslan2@hotmail.com>
2024-01-16 15:55:19 +0300

    reference swipe

--- api/models.py
+++ api/models.py
@@ -2,20 +2,52 @@ from django.db import models
 
 
 class Reffrences(models.Model):
-    category_choices = [
-        ["Yol ve Köprü İnşaatı", "Yol ve Köprü İnşaatı"],
-        ["Binalar", "Binalar"],
-        ["Bakım ve Onarım", "Bakım ve Onarım"],
-        ["Tesisat İnşaatı", "Tesisat İnşaatı"]
-    ]
+    # category_choices = [
+    #     ["Yol ve Köprü İnşaatı", "Yol ve Köprü İnşaatı"],
+    #     ["Binalar", "Binalar"],
+    #     ["Bakım ve Onarım", "Bakım ve Onarım"],
+    #     ["Tesisat İnşaatı", "Tesisat İnşaatı"],
+    # ]
 
-    baslik = models.CharField(max_length=150)
-    aciklama = models.TextField()
-    kategori = models.CharField(
-        max_length=50, choices=category_choices, default="Yol ve Köprü İnşaatı")
-    referans_fotografi = models.ImageField(
-        upload_to="media/reference_images/")
-    date = models.DateField(auto_now=False, auto_now_add=True)
+    baslik = models.CharField(
+        max_length=150,
+        null=False,
+    )
+    aciklama = models.TextField(max_length=400, null=True)
+    # kategori = models.CharField(
+    #     max_length=50, choices=category_choices, default="Yol ve Köprü İnşaatı"
+    # )
+    referans_fotografi_1 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_2 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_3 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_4 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_5 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_6 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_7 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_8 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_9 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    referans_fotografi_10 = models.ImageField(
+        default="", upload_to=f"media/reference_images/", null=False, blank=True
+    )
+    date = models.DateField(auto_now=False, auto_now_add=True, null=False)
 
     class Meta:
         verbose_name = "Referans Yonetimi"
--- frontend/views.py
+++ frontend/views.py
@@ -3,24 +3,34 @@ from api.models import Reffrences
 
 
 def home(request, *args, **kwargs):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def reference(request, *args, **kwargs):
-    the_value = "Yol ve Köprü İnşaatı"
-    reference_category = Reffrences.objects.filter(kategori=the_value)
-    print(reference_category)
+    reference_category = Reffrences.objects.values()
+    the_array = []
+    reference_len = len(reference_category)
+    for y in range(reference_len):
+        temp_array = []
+        for i in range(1, 10):
+            print(y)
+            the_value = reference_category[y][f"referans_fotografi_{i}"]
+            print(temp_array)
+            if the_value != "":
+                temp_array.append(the_value)
+        the_array.append(temp_array)
+        print(the_array)
 
-    return render(request, 'references.html', {'reference_category': reference_category})
+    return render(request, "references.html", {"reference_category": the_array})
 
 
 def about(request, *args, **kwargs):
-    return render(request, 'about.html')
+    return render(request, "about.html")
 
 
 def navbar(request, *args, **kwargs):
-    return render(request, 'navbar.html')
+    return render(request, "navbar.html")
 
 
 def contact(request, *args, **kwargs):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
